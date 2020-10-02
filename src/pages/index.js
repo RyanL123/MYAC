186c91e8a4bc6fd4f7c885359c3720260c79be25
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 21:04:39 -0400

    chore: import components into index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react"
 import { Box } from "@chakra-ui/core"
 import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import Objectives from "../components/Objectives"
 
 export default function Home() {
     return (
         <Box backgroundColor="gray.800">
             <Navbar />
+            <Hero />
+            <Objectives />
         </Box>
     )
 }
