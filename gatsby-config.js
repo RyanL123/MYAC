d018acac1fd9d2dc637d9938210eaf2e093dc5af
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 21:00:01 -0400

    chore: add gatsby-background-image and gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,16 @@
+const path = require(`path`)
+
 module.exports = {
-    plugins: ["gatsby-plugin-chakra-ui"],
+    plugins: [
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `assets`,
+                path: path.join(__dirname, `src`, `assets`),
+            },
+        },
+        `gatsby-plugin-chakra-ui`,
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+    ],
 }
