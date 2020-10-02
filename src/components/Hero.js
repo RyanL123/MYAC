25fad2175415da4377970b0378f6b0cc8a400061
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 21:00:48 -0400

    feat: hero background image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,13 +1,41 @@
 import React from "react"
 import { Box, Heading } from "@chakra-ui/core"
+import { graphql, useStaticQuery } from "gatsby"
+import BackgroundImage from "gatsby-background-image"
 
-export default () => (
-    <Box height="700px" px="10vw" py="50px" display="flex" alignItems="center">
-        <Box maxWidth="1200px">
-            <Heading color="white">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Heading>
+const Hero = () => {
+    const { hero } = useStaticQuery(graphql`
+        query {
+            hero: file(relativePath: { eq: "hero.jpg" }) {
+                childImageSharp {
+                    fluid(maxWidth: 1440, maxHeight: 900, quality: 100) {
+                        ...GatsbyImageSharpFluid_withWebp
+                    }
+                }
+            }
+        }
+    `)
+    return (
+        <Box height="700px">
+            <BackgroundImage
+                fluid={hero.childImageSharp.fluid}
+                style={{
+                    height: "100%",
+                    padding: "50px 10vw",
+                    display: "flex",
+                    alignItems: "center",
+                }}
+            >
+                <Box maxWidth="1200px">
+                    <Heading color="white">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua.
+                    </Heading>
+                </Box>
+            </BackgroundImage>
         </Box>
-    </Box>
-)
+    )
+}
+
+export default Hero
