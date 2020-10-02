21e528d943c0fa1f30e5b17f16471cddc3917db5
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 20:45:30 -0400

    feat: refine navbar elements

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,22 @@ import React from "react"
 import { Box, Text, Image } from "@chakra-ui/core"
 import Logo from "../assets/logo.png"
 
+const NavItem = ({ name, url }) => (
+    <Text color="gray.200" fontWeight="500">
+        {name}
+    </Text>
+)
+
 export default () => (
     <Box
         display="flex"
         flexDirection="row"
         width="100%"
-        py="20px"
+        py="24px"
         px="10vw"
         alignItems="center"
         justifyContent="space-between"
+        backgroundColor="gray.700"
     >
         <Box>
             <Image src={Logo} height="30px" width="auto" />
@@ -18,13 +25,13 @@ export default () => (
         <Box
             display="flex"
             flexDirection="row"
-            maxWidth="200px"
+            maxWidth="240px"
             width="100%"
             justifyContent="space-evenly"
         >
-            <Text>Home</Text>
-            <Text>About</Text>
-            <Text>Contact</Text>
+            <NavItem name="Home" />
+            <NavItem name="About" />
+            <NavItem name="Contact" />
         </Box>
     </Box>
 )
