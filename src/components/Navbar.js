3cd8e067a971cfae7d7a2d1b51fb7a4a107b5095
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 22:01:34 -0400

    feat: implement gatsby-image

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react"
-import { Box, Text, Image } from "@chakra-ui/core"
-import Logo from "../assets/logo.png"
+import { Box, Text } from "@chakra-ui/core"
+import { graphql, useStaticQuery } from "gatsby"
+import Img from "gatsby-image"
 
 const NavItem = ({ name, url }) => (
     <Text color="gray.200" fontWeight="500">
@@ -8,30 +9,45 @@ const NavItem = ({ name, url }) => (
     </Text>
 )
 
-export default () => (
-    <Box
-        display="flex"
-        flexDirection="row"
-        width="100%"
-        py="24px"
-        px="10vw"
-        alignItems="center"
-        justifyContent="space-between"
-        backgroundColor="gray.700"
-    >
-        <Box>
-            <Image src={Logo} height="30px" width="auto" />
-        </Box>
+const Navbar = () => {
+    const { logo } = useStaticQuery(graphql`
+        query {
+            logo: file(relativePath: { eq: "logo.png" }) {
+                childImageSharp {
+                    fixed(height: 30, quality: 100) {
+                        ...GatsbyImageSharpFixed_withWebp
+                    }
+                }
+            }
+        }
+    `)
+    return (
         <Box
             display="flex"
             flexDirection="row"
-            maxWidth="240px"
             width="100%"
-            justifyContent="space-evenly"
+            py="24px"
+            px="10vw"
+            alignItems="center"
+            justifyContent="space-between"
+            backgroundColor="gray.700"
         >
-            <NavItem name="Home" />
-            <NavItem name="About" />
-            <NavItem name="Contact" />
+            <Box>
+                <Img fixed={logo.childImageSharp.fixed} />
+            </Box>
+            <Box
+                display="flex"
+                flexDirection="row"
+                maxWidth="240px"
+                width="100%"
+                justifyContent="space-evenly"
+            >
+                <NavItem name="Home" />
+                <NavItem name="About" />
+                <NavItem name="Contact" />
+            </Box>
         </Box>
-    </Box>
-)
+    )
+}
+
+export default Navbar
