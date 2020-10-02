5a5758722fe9b9ceb9a9b24d7c90ccc8b906bf37
Ryan <ryan.lichangrui@gmail.com>
2020-10-01 20:45:00 -0400

    feat: implement hero

---
+++ src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from "react"
+import { Box, Heading } from "@chakra-ui/core"
+
+export default () => (
+    <Box height="700px" px="10vw" py="50px" display="flex" alignItems="center">
+        <Box maxWidth="1200px">
+            <Heading color="white">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Heading>
+        </Box>
+    </Box>
+)
