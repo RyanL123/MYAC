ba4ad7c90a294f165117576da7de3488275d6211
Ryan <ryan.lichangrui@gmail.com>
2020-10-10 15:48:53 -0400

    feat: remove hard coded color on title components

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,12 @@
 import React from "react"
 import { Box, Heading } from "@chakra-ui/core"
 
-export default ({ title, subtitle, align }) => (
+export default ({ title, subtitle, align, subColor, color }) => (
     <Box textAlign={align}>
-        <Heading color="blue.200" size="md" fontWeight="500">
+        <Heading color={subColor} size="md" fontWeight="500">
             {subtitle}
         </Heading>
-        <Heading color="white" mt="10px" size="xl">
+        <Heading color={color} mt="10px" size="2xl">
             {title}
         </Heading>
     </Box>
