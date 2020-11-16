1567cc3a1767519d2ce7a387b98feadb9b29d8f9
Ryan <ryan.lichangrui@gmail.com>
2020-11-15 21:15:58 -0500

    feat: show navbar on scrollup

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,18 @@
 import React from "react"
-import { Box, Text } from "@chakra-ui/core"
+import { Box, PseudoBox, Text } from "@chakra-ui/core"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import Img from "gatsby-image"
+import Headroom from "react-headroom"
 
 const NavItem = ({ name, url }) => (
     <Link to={url}>
-        <Text color="gray.200" fontWeight="500">
-            {name}
-        </Text>
+        <PseudoBox
+            color="gray.200"
+            transition="all .25s ease-in-out"
+            _hover={{ color: "gray.50" }}
+        >
+            <Text fontWeight="500">{name}</Text>
+        </PseudoBox>
     </Link>
 )
 
@@ -24,32 +29,41 @@ const Navbar = () => {
         }
     `)
     return (
-        <Box
-            display="flex"
-            flexDirection="row"
-            width="100%"
-            py="24px"
-            px="10vw"
-            alignItems="center"
-            justifyContent="space-between"
-            backgroundColor="gray.800"
-            borderBottom="1px solid hsla(0,0%,100%,0.2)"
+        <Headroom
+            style={{
+                webkitTransition: "all .6s ease-in-out",
+                mozTransition: "all .6s ease-in-out",
+                oTransition: "all .6s ease-in-out",
+                transition: "all .6s ease-in-out",
+            }}
         >
-            <Box>
-                <Img fixed={logo.childImageSharp.fixed} />
-            </Box>
             <Box
                 display="flex"
                 flexDirection="row"
-                maxWidth="240px"
                 width="100%"
-                justifyContent="space-evenly"
+                py="24px"
+                px="10vw"
+                alignItems="center"
+                justifyContent="space-between"
+                backgroundColor="gray.900"
+                borderBottom="1px solid hsla(0,0%,100%,0.2)"
             >
-                <NavItem name="Home" url="/" />
-                <NavItem name="About" url="/about" />
-                <NavItem name="Events" url="/events" />
+                <Box>
+                    <Img fixed={logo.childImageSharp.fixed} />
+                </Box>
+                <Box
+                    display="flex"
+                    flexDirection="row"
+                    maxWidth="240px"
+                    width="100%"
+                    justifyContent="space-evenly"
+                >
+                    <NavItem name="Home" url="/" />
+                    <NavItem name="About" url="/about" />
+                    <NavItem name="Events" url="/events" />
+                </Box>
             </Box>
-        </Box>
+        </Headroom>
     )
 }
 
