d9d62dbb697fcc63666db9f2db6e86654ff74c83
Aritro <29025984+AritroSaha10@users.noreply.github.com>
2021-08-31 12:39:15 -0400

    feat: refactored councils page

---
+++ src/components/councils/CouncilCard.js
@@ -0,0 +1,93 @@
+import React from "react"
+
+import {
+    Box,
+    Heading,
+    Text,
+    Image,
+    Button,
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverArrow,
+    PopoverCloseButton,
+} from "@chakra-ui/react"
+import { ExternalLinkIcon } from "@chakra-ui/icons"
+
+export default function Council({ title, desc, img, link, contact }) {
+    return (
+        <Box
+            shadow="xl"
+            p="24px"
+            backgroundColor="gray.50"
+            border="1px rgb(0, 0, 0, 0.1) solid"
+            borderRadius="lg"
+            display="flex"
+            flexDirection="row"
+            alignItems="center"
+        >
+            <Image
+                src={img}
+                height="200px"
+                width="200px"
+                objectFit="cover"
+                borderRadius="md"
+                alt={title}
+            />
+            <Box
+                ml="20px"
+                display="flex"
+                flexDirection="column"
+                justifyContent="space-between"
+                height="100%"
+            >
+                <Box>
+                    <Heading color="black.900" size="lg">
+                        {title}
+                    </Heading>
+                    <Box
+                        display="flex"
+                        flexDirection="row"
+                        alignItems="center"
+                        mt="10px"
+                    ></Box>
+                    <Text color="gray.500" fontSize="lg" fontWeight="500" mt="10px">
+                        {desc}
+                    </Text>
+                </Box>
+                <Box mt="10px">
+                    <Button
+                        variant="link"
+                        colorScheme="blue"
+                        rightIcon={<ExternalLinkIcon />}
+                    >
+                        <a
+                            href={link}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            aria-label="Learn More"
+                        >
+                            Learn More
+                        </a>
+                    </Button>
+                    <Popover>
+                        <PopoverTrigger>
+                            <Button ml="20px" backgroundColor="blue.400" color="white" _hover={{
+                                background: "blue.600",
+                                color: "white"
+                            }}>Contact</Button>
+                        </PopoverTrigger>
+                        <PopoverContent zIndex={4} width="500px">
+                            <PopoverArrow />
+                            <PopoverCloseButton />
+                            <PopoverHeader>Contact Us</PopoverHeader>
+                            <PopoverBody>{contact}</PopoverBody>
+                        </PopoverContent>
+                    </Popover>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
--- src/pages/councils.js
+++ src/pages/councils.js
@@ -1,115 +1,12 @@
 import React, { useState } from "react"
 import { graphql, useStaticQuery } from "gatsby"
-import {
-    Box,
-    Heading,
-    Text,
-    Image,
-    Button,
-    Skeleton,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    PopoverHeader,
-    PopoverBody,
-    PopoverArrow,
-    PopoverCloseButton,
-} from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react"
 import MySEO from "../components/SEO"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
-import { ExternalLinkIcon } from "@chakra-ui/icons"
 
-const Event = ({ title, desc, img, link, contact }) => (
-    <Box
-        shadow="xl"
-        p="24px"
-        backgroundColor="gray.50"
-        border="1px rgb(0, 0, 0, 0.1) solid"
-        borderRadius="lg"
-        display="flex"
-        flexDirection="row"
-        alignItems="center"
-    >
-        <Image
-            src={img}
-            height="200px"
-            width="200px"
-            objectFit="cover"
-            borderRadius="md"
-            alt={title}
-        />
-        <Box
-            ml="20px"
-            display="flex"
-            flexDirection="column"
-            justifyContent="space-between"
-            height="100%"
-        >
-            <Box>
-                <Heading color="black.900" size="lg">
-                    {title}
-                </Heading>
-                <Box
-                    display="flex"
-                    flexDirection="row"
-                    alignItems="center"
-                    mt="10px"
-                ></Box>
-                <Text color="gray.500" fontSize="lg" fontWeight="500" mt="10px">
-                    {desc}
-                </Text>
-            </Box>
-            <Box mt="10px">
-                <Button
-                    variant="link"
-                    colorScheme="blue"
-                    rightIcon={<ExternalLinkIcon />}
-                >
-                    <a
-                        href={link}
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        aria-label="Learn More"
-                    >
-                        Learn More
-                    </a>
-                </Button>
-                <Popover>
-                    <PopoverTrigger>
-                        <Button ml="20px" backgroundColor="blue.400" color="white" _hover={{
-                            background: "blue.600",
-                            color: "white"
-                        }}>Contact</Button>
-                    </PopoverTrigger>
-                    <PopoverContent zIndex={4} width="500px">
-                        <PopoverArrow />
-                        <PopoverCloseButton />
-                        <PopoverHeader>Contact Us</PopoverHeader>
-                        <PopoverBody>{contact}</PopoverBody>
-                    </PopoverContent>
-                </Popover>
-            </Box>
-        </Box>
-    </Box>
-)
-
-const Placeholder = () => (
-    <Box
-        shadow="lg"
-        p="24px"
-        backgroundColor="white"
-        borderRadius="lg"
-        display="flex"
-        flexDirection="column"
-    >
-        <Skeleton height="200px" width="100%" />
-        <Box mt="20px">
-            <Skeleton height="28px" />
-            <Skeleton height="20px" />
-        </Box>
-    </Box>
-)
+import Council from "../components/councils/CouncilCard"
+import Placeholder from "../components/councils/CouncilPlaceholder"
 
 const placeholders = Array(8).fill(<Placeholder />)
 
@@ -133,13 +30,15 @@ const Events = () => {
             }
         }
     `)
+
     const [events, setEvents] = useState([])
     const [loading, setLoading] = useState(true)
+
     if (loading) {
         // Gets airtable data then sorts based on date
         var qldata = data["allAirtable"]["edges"].map(event => ({
             component: (
-                <Event
+                <Council
                     title={event.node.data.Name}
                     desc={event.node.data.Description}
                     img={
@@ -160,6 +59,7 @@ const Events = () => {
         setEvents(qldata)
         setLoading(false)
     }
+
     return (
         <Box backgroundColor="white">
             <MySEO title="MYAC | Events" />
